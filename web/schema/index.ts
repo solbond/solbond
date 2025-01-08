a121f9fd723555a562a9ddb7d7d27c28dae8373d
Nikita <github@nikiv.dev>
2025-01-08 18:44:51 +0100

    ronin

--- web/schema/index.ts
+++ web/schema/index.ts
@@ -1,4 +1,4 @@
-import { link, model, string, boolean, number } from "ronin/schema"
+import { link, model, string, boolean, number, blob } from "ronin/schema"
 
 export const UserModel = model({
   slug: "user",
@@ -23,5 +23,17 @@ export const ProductModel = model({
     createdBy: link({
       target: "user",
     }),
+  },
+})
+
+export const FileModel = model({
+  slug: "product",
+  fields: {
+    partOfProduct: link({
+      target: "product",
+    }),
+    content: blob(),
+    name: string(),
+    description: string(),
   },
 })
