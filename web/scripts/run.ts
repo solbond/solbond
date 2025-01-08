ee43c96629cd0a12b9b9922b1f330a5557f199ee
Nikita <github@nikiv.dev>
2025-01-08 18:01:58 +0100

    ronin

--- web/scripts/run.ts
+++ web/scripts/run.ts
@@ -1,5 +1,14 @@
+import ronin from "ronin"
+import * as models from "../schema/index"
+const { add } = ronin({ models })
+
 async function main() {
-  console.log("test")
+  const res = await add.user.to({
+    email: "nikita@nikiv.dev",
+    clerkId: "user_2Q5678901234567890123456",
+    username: "nikiv",
+  })
+  console.log(res)
 }
 
 main()
