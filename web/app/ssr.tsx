11ab68415a77feb92180d54bf79024294d8d280b
Nikita <github@nikiv.dev>
2025-01-09 21:37:51 +0100

    no clerk

--- web/app/ssr.tsx
+++ web/app/ssr.tsx
@@ -5,11 +5,8 @@ import {
 } from "@tanstack/start/server"
 import { getRouterManifest } from "@tanstack/start/router-manifest"
 import { createRouter } from "./router"
-import { createClerkHandler } from "@clerk/tanstack-start/server"
 
-export default createClerkHandler(
-  createStartHandler({
-    createRouter,
-    getRouterManifest,
-  }),
-)(defaultStreamHandler)
+export default createStartHandler({
+  createRouter,
+  getRouterManifest,
+})(defaultStreamHandler)
