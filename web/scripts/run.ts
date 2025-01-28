4f83f186789bf4745017d424f14b6370146be6be
Nikita <github@nikiv.dev>
2025-01-28 17:22:35 +0100

    .

--- web/scripts/run.ts
+++ web/scripts/run.ts
@@ -1,3 +1,25 @@
-async function main() {}
+import { get, add, remove } from "ronin"
 
-main()
+async function main() {
+  // await run()
+  await createUser()
+  // await resetData()
+}
+
+async function resetData() {
+  await remove.users()
+}
+
+async function createUser() {
+  // @ts-ignore
+  const user = await add.user.with({
+    email: "nikita@nikiv.dev",
+    username: "nikiv",
+  })
+  console.log(user)
+}
+
+async function run() {}
+
+await main()
+console.log("done")
