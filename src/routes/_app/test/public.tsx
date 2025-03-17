b0cc637add8d8fc4135519573fb8f5ce35f4bafd
Nikita <github@nikiv.dev>
2025-03-17 20:50:53 +0100

    auth

--- src/routes/_app/test/public.tsx
+++ src/routes/_app/test/public.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { useIsAuthenticated } from "jazz-react"
 
+// TODO: figure out how to not trigger auth on public pages
+// query jazz state?
 function RouteComponent() {
+  const authed = useIsAuthenticated()
+  console.log(authed, "authed")
   return (
     <>
-      <div>Anyone can see this</div>
+      <div>Public page, no need to sign in yet</div>
     </>
   )
 }
