1d0a4664e1164f990ab2913a53b74dc98eedb228
Nikita <github@nikiv.dev>
2025-03-26 13:42:28 +0100

    no auth for now

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, Outlet } from "@tanstack/react-router"
 import { JazzInspector } from "jazz-inspector"
 import { JazzProvider } from "jazz-react"
 import { JazzAccount } from "~/jazz/schema"
-import { CloudAuthBasicUI } from "jazz-react-auth-cloudauth"
+// import { CloudAuthBasicUI } from "jazz-react-auth-cloudauth"
 
 export const APPLICATION_NAME = "SolBond"
 
@@ -22,22 +22,22 @@ function LayoutComponent() {
         }}
         AccountSchema={JazzAccount}
       >
-        <CloudAuthBasicUI
+        {/* <CloudAuthBasicUI
           appName="SolBond"
           baseUrl="http://localhost:3010"
           keyserver="http://localhost:6189"
-        >
-          <main className="min-h-screen flex flex-col">
-            <div className="flex flex-col min-h-screen w-full pt-14">
-              <div className="flex flex-1 w-full">
-                <Outlet />
-              </div>
+        > */}
+        <main className="min-h-screen flex flex-col">
+          <div className="flex flex-col min-h-screen w-full pt-14">
+            <div className="flex flex-1 w-full">
+              <Outlet />
             </div>
-          </main>
-          <div className="hidden">
-            <JazzInspector />
           </div>
-        </CloudAuthBasicUI>
+        </main>
+        <div className="hidden">
+          <JazzInspector />
+        </div>
+        {/* </CloudAuthBasicUI> */}
       </JazzProvider>
     </>
   )
