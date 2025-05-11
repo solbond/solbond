00cfd0c93a0031a93563ad324db3270d0c064630
marshennikovaolga <marshennikova@gmail.com>
2025-05-11 14:53:34 +0200

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+"use client"
+
+import Sidebar from "../components/Sidebar"
+import Main from "../components/Main"
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-red-100">index</h1>
+    <div className="w-full py-4 px-2 flex flex-row h-screen bg-[#0f0f0f]">
+      <Sidebar />
+      <div className="w-full h-full bg-[#161616] border border-white/10 rounded-xl p-4">
+        <Main />
+      </div>
     </div>
   )
 }
