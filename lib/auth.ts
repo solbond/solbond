40cca654fc153e54c7bbf312ff1a676d76f5e921
Leo Lamprecht <leo@ronin.co>
2025-05-16 14:44:33 +0200

    Added Better Auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,13 @@
 import { betterAuth } from "better-auth"
 import { ronin } from "@ronin/better-auth"
 
-const auth = betterAuth({
+export const auth = betterAuth({
   database: ronin(),
 })
+
+export type AuthType = {
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null
+    session: typeof auth.$Infer.Session.session | null
+  }
+}
