a279d958e8c962dc10af7cc344100f4bcb5a19e9
marshennikovaolga <marshennikova@gmail.com>
2025-05-17 13:32:33 +0300

    .

---
+++ pages/analytics/index.tsx
@@ -0,0 +1,15 @@
+import Layout from "../../components/layout"
+
+const Page = () => {
+  return (
+    <Layout>
+      <div className="flex flex-row justify-between items-center">
+        <h1 className="text-white text-xl font-normal tracking-wider">
+          Analytics
+        </h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default Page
